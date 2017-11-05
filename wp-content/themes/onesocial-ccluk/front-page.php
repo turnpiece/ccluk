574cc8dced8798a5b5e91529dd6aee24aa42f2f9
Paul Jenkins <paul@turnpiece.com>
2017-11-05 21:43:03 +0000

    news posts on home page

--- wp-content/themes/onesocial-ccluk/front-page.php
+++ wp-content/themes/onesocial-ccluk/front-page.php
@@ -30,9 +30,31 @@ get_header();
 	);
 
 	if ( $q->have_posts() ) : ?>
-<section id="posts" class="section site-content">
+<section id="posts" class="section site-content posts">
 	<div class="section-title">
 		<h4><?php _e( "Views", '' ) ?></h4>
+	</div>
+	<div class="section-content">
+		<?php while( $q->have_posts() ) : $q->the_post(); ?>
+		<?php get_template_part( 'template-parts/content', 'list' ); ?>
+		<?php endwhile; wp_reset_postdata(); ?>
+	</div>
+</section>
+<?php endif; ?>
+
+<!-- get latest three news articles -->
+<?php
+	$q = new WP_Query(
+		array(
+			'posts_per_page' => 3,
+			'post_type' => 'ccluk_news'
+		)
+	);
+
+	if ( $q->have_posts() ) : ?>
+<section id="news" class="section site-content posts">
+	<div class="section-title">
+		<h4><?php _e( "News", '' ) ?></h4>
 	</div>
 	<div class="section-content">
 		<?php while( $q->have_posts() ) : $q->the_post(); ?>
