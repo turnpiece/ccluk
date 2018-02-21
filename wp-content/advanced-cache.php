bb1793fd5be01705f1d4001fa2657ef473a9017a
Paul Jenkins <paul@turnpiece.com>
2018-02-21 18:01:01 +0000

    updated wordpress

---
+++ wp-content/advanced-cache.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Hummingbird Page Caching
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
+
+/**
+ * Load necessary modules for caching.
+ */
+
+if ( ! class_exists( 'WP_Hummingbird_Module_Page_Caching' ) ) {
+	if ( is_dir( WP_CONTENT_DIR . '/plugins/wp-hummingbird/' ) ) {
+		$path = WP_CONTENT_DIR . '/plugins/wp-hummingbird/';
+	} elseif ( is_dir( WP_CONTENT_DIR . '/plugins/hummingbird-performance/' ) ) {
+		$path = WP_CONTENT_DIR . '/plugins/hummingbird-performance/';
+	} else {
+		$path = WP_CONTENT_DIR . '/plugins/wp-hummingbird-wporg/';
+	}
+
+	include_once( $path . 'helpers/wp-hummingbird-helpers-core.php' );
+	include_once( $path . 'core/class-abstract-module.php' );
+	include_once( $path . 'core/modules/class-module-page-caching.php' );
+
+	if ( ! method_exists( 'WP_Hummingbird_Module_Page_Caching', 'serve_cache' ) ) {
+		return;
+	}
+
+	define( 'WPHB_ADVANCED_CACHE', true );
+	WP_Hummingbird_Module_Page_Caching::serve_cache();
+}
