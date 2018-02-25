3b3c7b1f06e981323739e3911ced4ea75e848b32
Paul Jenkins <paul@turnpiece.com>
2018-02-25 11:22:50 +0000

    OnePress theme to customize home page

---
+++ wp-content/themes/onepress/template-frontpage.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *Template Name: Frontpage
+ *
+ * @package OnePress
+ */
+
+get_header(); ?>
+
+	<div id="content" class="site-content">
+		<main id="main" class="site-main" role="main">
+            <?php
+
+            do_action( 'onepress_frontpage_before_section_parts' );
+
+			if ( ! has_action( 'onepress_frontpage_section_parts' ) ) {
+
+				$sections = apply_filters( 'onepress_frontpage_sections_order', array(
+                    'features', 'about', 'services', 'videolightbox', 'gallery', 'counter', 'team',  'news', 'contact'
+                ) );
+
+				foreach ( $sections as $section ){
+                    onepress_load_section( $section );
+				}
+
+			} else {
+				do_action( 'onepress_frontpage_section_parts' );
+			}
+
+            do_action( 'onepress_frontpage_after_section_parts' );
+
+			?>
+		</main><!-- #main -->
+	</div><!-- #content -->
+
+<?php get_footer(); ?>
