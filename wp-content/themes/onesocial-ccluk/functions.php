0c56a3575b01e26cbeeb4c7e7c7e9c8735e1af3d
Paul Jenkins <paul@turnpiece.com>
2018-03-10 19:31:53 +0000

    ensure child theme stylesheet loads after those of the parent theme

--- wp-content/themes/onesocial-ccluk/functions.php
+++ wp-content/themes/onesocial-ccluk/functions.php
@@ -74,8 +74,11 @@ function ccluk_theme_scripts_styles()
 
   /*
    * Styles
+   *
+   * need to ensure this stylesheet loads after the parent stylesheets
+   *
    */
-  wp_enqueue_style( 'onesocial-ccluk-custom', get_stylesheet_directory_uri().'/assets/css/custom.'.(CCLUK_DEBUGGING ? '' : 'min.').'css' );
+  wp_enqueue_style( 'onesocial-ccluk-custom', get_stylesheet_directory_uri().'/assets/css/custom.'.(CCLUK_DEBUGGING ? '' : 'min.').'css', array( 'onesocial-main-global-css', 'onesocial-main-desktop-css', 'onesocial-main-mobile-css') );
 
 }
 add_action( 'wp_enqueue_scripts', 'ccluk_theme_scripts_styles', 9999 );
