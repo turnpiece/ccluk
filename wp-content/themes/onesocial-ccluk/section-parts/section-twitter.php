49fb4292d38c552a1d3ea33ad0c5fe8103e48fbb
Paul Jenkins <paul@turnpiece.com>
2018-04-21 21:24:24 +0100

    styling twitter section

--- wp-content/themes/onesocial-ccluk/section-parts/section-twitter.php
+++ wp-content/themes/onesocial-ccluk/section-parts/section-twitter.php
@@ -1,5 +1,5 @@
 <?php if (function_exists('ctf_init')) : ?>
-<section id="twitter_feed" class="section twitter site-content">
+<section id="twitter_feed" class="section twitter site-content blue-bg">
 	<div class="section-title">
         <h4><?php _e( "Tweets", 'onesocial' ) ?></h4>
     </div>
