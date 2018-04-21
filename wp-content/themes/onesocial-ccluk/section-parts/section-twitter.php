62553cc12afb00ab7d943ff631c035371369e6a8
Paul Jenkins <paul@turnpiece.com>
2018-04-21 21:02:05 +0100

    home page styling tweaks

--- wp-content/themes/onesocial-ccluk/section-parts/section-twitter.php
+++ wp-content/themes/onesocial-ccluk/section-parts/section-twitter.php
@@ -1,5 +1,8 @@
 <?php if (function_exists('ctf_init')) : ?>
 <section id="twitter_feed" class="section twitter site-content">
+	<div class="section-title">
+        <h4><?php _e( "Tweets", 'onesocial' ) ?></h4>
+    </div>
     <?php echo do_shortcode('[custom-twitter-feeds]') ?>
 </section>
 <?php endif; ?>
