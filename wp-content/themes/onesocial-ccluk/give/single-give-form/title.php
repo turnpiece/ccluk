a0aac4f0cb50fb4b423d06b04d6f87c7d4602eae
Paul Jenkins <paul@turnpiece.com>
2018-07-28 14:32:17 +0100

    give donation form styling

---
+++ wp-content/themes/onesocial-ccluk/give/single-give-form/title.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Single Give Form Title
+ * 
+ * Displays the main title for the single donation form - Override this template by copying it to yourtheme/give/single-give-form/title.php
+ * 
+ * @package     Give
+ * @subpackage  templates/single-give-form
+ * @copyright   Copyright (c) 2016, WordImpress
+ * @license     https://opensource.org/licenses/gpl-license GNU Public License
+ * @since       1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+} ?>
