50d0f288aa31071facc9f59edfc61c7318c5a4b0
Paul Jenkins <paul@turnpiece.com>
2018-08-03 23:06:59 +0100

    compose message form template

---
+++ wp-content/themes/onesocial-ccluk/buddypress/members/single/messages/compose.php
@@ -0,0 +1,22 @@
+<form action="<?php bp_messages_form_action('compose' ); ?>" method="post" id="send_message_form" class="standard-form" role="main" enctype="multipart/form-data">
+
+	<?php do_action( 'bp_before_messages_compose_content' ); ?>
+
+	<?php bp_message_get_recipient_tabs(); ?>
+
+	<label for="subject"><?php _e( 'Subject', 'onesocial' ); ?></label>
+	<input type="text" name="subject" id="subject" value="<?php bp_messages_subject_value(); ?>" />
+
+	<label for="content"><?php _e( 'Message', 'onesocial' ); ?></label>
+	<textarea name="content" id="message_content" rows="15" cols="40"><?php bp_messages_content_value(); ?></textarea>
+
+	<input type="hidden" name="send_to_usernames" id="send-to-usernames" value="<?php bp_message_get_recipient_usernames(); ?>" class="<?php bp_message_get_recipient_usernames(); ?>" />
+
+	<?php do_action( 'bp_after_messages_compose_content' ); ?>
+
+	<div class="submit">
+		<input type="submit" value="<?php _e( "Send Message", 'onesocial' ); ?>" name="send" id="send" />
+	</div>
+
+	<?php wp_nonce_field( 'messages_send_message' ); ?>
+</form>
