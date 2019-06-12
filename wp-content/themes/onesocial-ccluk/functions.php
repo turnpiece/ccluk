ca4687ae08c464a248c09ec926ab7f67a6258c35
Paul Jenkins <paul@turnpiece.com>
2019-06-12 22:34:02 +0100

    added BuddyPress filter to send profile data to Mailchimp

--- wp-content/themes/onesocial-ccluk/functions.php
+++ wp-content/themes/onesocial-ccluk/functions.php
@@ -846,3 +846,14 @@ add_action( 'bp_after_registration_submit_buttons', function() {
     if ( 'registration-disabled' != bp_get_current_signup_step() )
         do_action( 'wordpress_social_login' );
 } );
+
+/**
+ * Add custom BuddyPress registration fields to MailChimp.
+ *
+ * - Replace "CUST1" with your MailChimp field name.
+ * - Replace "13" with your BuddyPress field ID.
+ */
+add_filter( 'mc4wp_integration_buddypress_data', function( $data, $user_id ) {
+    $data['CUST1'] = xprofile_get_field_data( 13 , $user_id );
+    return $data;
+}, 10, 2);
