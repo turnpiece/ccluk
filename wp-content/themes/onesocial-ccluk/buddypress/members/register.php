3a7cdb0e66a67bb9652f4230093e07d10321a432
Paul Jenkins <paul@turnpiece.com>
2019-06-18 21:39:43 +0100

    note with postcode field

--- wp-content/themes/onesocial-ccluk/buddypress/members/register.php
+++ wp-content/themes/onesocial-ccluk/buddypress/members/register.php
@@ -204,14 +204,19 @@
 									<button type="button" class="field-visibility-settings-close"><?php _e( 'Close', 'buddypress' ) ?></button>
 
 								</div>
+
 							<?php else : ?>
 								<p class="field-visibility-settings-notoggle" id="field-visibility-settings-toggle-<?php bp_the_profile_field_id() ?>">
-									<?php
-									printf(
-										__( 'This field can be seen by: %s', 'buddypress' ),
-										'<span class="current-visibility-level">' . bp_get_the_profile_field_visibility_level_label() . '</span>'
-									);
-									?>
+									<?php if ((int)bp_get_the_profile_field_id() == 2) { // postcode field
+										_e( "Your postcode will not be displayed to other people but will be used to get the constituency you're in and the name of your MP.", 'onesocial' );
+
+									} else {
+										printf(
+											__( 'This field can be seen by: %s', 'buddypress' ),
+											'<span class="current-visibility-level">' . bp_get_the_profile_field_visibility_level_label() . '</span>'
+										);
+
+									} ?>
 								</p>
 							<?php endif ?>
 
@@ -252,93 +257,6 @@
 				 * @since 1.1.0
 				 */
 				do_action( 'bp_after_signup_profile_fields' ); ?>
-
-			<?php endif; ?>
-
-			<?php if ( bp_get_blog_signup_allowed() ) : ?>
-
-				<?php
-
-				/**
-				 * Fires before the display of member registration blog details fields.
-				 *
-				 * @since 1.1.0
-				 */
-				do_action( 'bp_before_blog_details_fields' ); ?>
-
-				<?php /***** Blog Creation Details ******/ ?>
-
-				<div class="register-section" id="blog-details-section">
-
-					<h2><?php _e( 'Blog Details', 'buddypress' ); ?></h2>
-
-					<p><label for="signup_with_blog"><input type="checkbox" name="signup_with_blog" id="signup_with_blog" value="1"<?php if ( (int) bp_get_signup_with_blog_value() ) : ?> checked="checked"<?php endif; ?> /> <?php _e( 'Yes, I\'d like to create a new site', 'buddypress' ); ?></label></p>
-
-					<div id="blog-details"<?php if ( (int) bp_get_signup_with_blog_value() ) : ?>class="show"<?php endif; ?>>
-
-						<label for="signup_blog_url"><?php _e( 'Blog URL', 'buddypress' ); ?> <?php _e( '(required)', 'buddypress' ); ?></label>
-						<?php
-
-						/**
-						 * Fires and displays any member registration blog URL errors.
-						 *
-						 * @since 1.1.0
-						 */
-						do_action( 'bp_signup_blog_url_errors' ); ?>
-
-						<?php if ( is_subdomain_install() ) : ?>
-							http:// <input type="text" name="signup_blog_url" id="signup_blog_url" value="<?php bp_signup_blog_url_value(); ?>" /> .<?php bp_signup_subdomain_base(); ?>
-						<?php else : ?>
-							<?php echo home_url( '/' ); ?> <input type="text" name="signup_blog_url" id="signup_blog_url" value="<?php bp_signup_blog_url_value(); ?>" />
-						<?php endif; ?>
-
-						<label for="signup_blog_title"><?php _e( 'Site Title', 'buddypress' ); ?> <?php _e( '(required)', 'buddypress' ); ?></label>
-						<?php
-
-						/**
-						 * Fires and displays any member registration blog title errors.
-						 *
-						 * @since 1.1.0
-						 */
-						do_action( 'bp_signup_blog_title_errors' ); ?>
-						<input type="text" name="signup_blog_title" id="signup_blog_title" value="<?php bp_signup_blog_title_value(); ?>" />
-
-						<fieldset class="register-site">
-							<legend class="label"><?php _e( 'Privacy: I would like my site to appear in search engines, and in public listings around this network.', 'buddypress' ); ?></legend>
-							<?php
-
-							/**
-							 * Fires and displays any member registration blog privacy errors.
-							 *
-							 * @since 1.1.0
-							 */
-							do_action( 'bp_signup_blog_privacy_errors' ); ?>
-
-							<label for="signup_blog_privacy_public"><input type="radio" name="signup_blog_privacy" id="signup_blog_privacy_public" value="public"<?php if ( 'public' == bp_get_signup_blog_privacy_value() || !bp_get_signup_blog_privacy_value() ) : ?> checked="checked"<?php endif; ?> /> <?php _e( 'Yes', 'buddypress' ); ?></label>
-							<label for="signup_blog_privacy_private"><input type="radio" name="signup_blog_privacy" id="signup_blog_privacy_private" value="private"<?php if ( 'private' == bp_get_signup_blog_privacy_value() ) : ?> checked="checked"<?php endif; ?> /> <?php _e( 'No', 'buddypress' ); ?></label>
-						</fieldset>
-
-						<?php
-
-						/**
-						 * Fires and displays any extra member registration blog details fields.
-						 *
-						 * @since 1.9.0
-						 */
-						do_action( 'bp_blog_details_fields' ); ?>
-
-					</div>
-
-				</div><!-- #blog-details-section -->
-
-				<?php
-
-				/**
-				 * Fires after the display of member registration blog details fields.
-				 *
-				 * @since 1.1.0
-				 */
-				do_action( 'bp_after_blog_details_fields' ); ?>
 
 			<?php endif; ?>
 
