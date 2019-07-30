305e41f18dbdfe6d2da3a61bbb25460be77f4679
Paul Jenkins <paul@turnpiece.com>
2019-07-30 21:33:15 +0100

    fixing profile templates and dealing with an empty profile

---
+++ wp-content/themes/onesocial-ccluk/buddypress/members/single/profile/profile-loop.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * BuddyPress - Members Profile Loop
+ *
+ * @package BuddyPress
+ * @subpackage bp-legacy
+ * @version 3.0.0
+ */
+
+/** This action is documented in bp-templates/bp-legacy/buddypress/members/single/profile/profile-wp.php */
+do_action( 'bp_before_profile_loop_content' ); ?>
+
+<?php if ( bp_has_profile() ) : $fields = 0; ?>
+
+	<?php while ( bp_profile_groups() ) : bp_the_profile_group(); ?>
+
+		<?php if ( bp_profile_group_has_fields() ) : ?>
+
+			<?php
+
+			/** This action is documented in bp-templates/bp-legacy/buddypress/members/single/profile/profile.php */
+			do_action( 'bp_before_profile_field_content' ); ?>
+
+			<div class="bp-widget <?php bp_the_profile_group_slug(); ?>">
+
+				<h2><?php bp_the_profile_group_name(); ?></h2>
+
+				<table class="profile-fields">
+
+					<?php while ( bp_profile_fields() ) : bp_the_profile_field(); ?>
+
+						<?php if ( bp_field_has_data() ) : $fields++; ?>
+
+							<tr<?php bp_field_css_class(); ?>>
+
+								<td class="label"><?php bp_the_profile_field_name(); ?></td>
+
+								<td class="data"><?php bp_the_profile_field_value(); ?></td>
+
+							</tr>
+
+						<?php endif; ?>
+
+						<?php
+
+						/**
+						 * Fires after the display of a field table row for profile data.
+						 *
+						 * @since 1.1.0
+						 */
+						do_action( 'bp_profile_field_item' ); ?>
+
+					<?php endwhile; ?>
+
+				</table>
+
+			</div>
+
+			<?php
+
+			/** This action is documented in bp-templates/bp-legacy/buddypress/members/single/profile/profile-wp.php */
+			do_action( 'bp_after_profile_field_content' ); ?>
+
+		<?php endif; ?>
+
+	<?php endwhile; ?>
+
+	<?php if ($fields <= 1) : ?>
+		<div class="message">
+			<?php if (bp_displayed_user_id() === bp_loggedin_user_id()) : ?>
+				<p><?php _e( "You haven't filled out your profile yet. Please tell us a bit about yourself.", 'onesocial' ) ?></p>
+			<?php else : ?>
+				<p><?php _e( 'This member has not filled out their profile.', 'onesocial' ) ?></p>
+			<?php endif; ?>
+		</div>
+	<?php endif; ?>
+
+	<?php
+
+	/** This action is documented in bp-templates/bp-legacy/buddypress/members/single/profile/profile-wp.php */
+	do_action( 'bp_profile_field_buttons' ); ?>
+
+<?php endif; ?>
+
+<?php
+
+/** This action is documented in bp-templates/bp-legacy/buddypress/members/single/profile/profile-wp.php */
+do_action( 'bp_after_profile_loop_content' );
