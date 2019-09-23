a8dfcd543be5a43b6ac8f4eca118efe498aadba1
Paul Jenkins <paul@turnpiece.com>
2019-09-23 21:04:14 +0100

    using Twitter's own widget instead of plugin

--- wp-content/themes/onesocial-ccluk/section-parts/section-twitter.php
+++ wp-content/themes/onesocial-ccluk/section-parts/section-twitter.php
@@ -1,8 +1,10 @@
-<?php if (function_exists('ctf_init')) : ?>
 <section id="twitter_feed" class="section twitter site-content blue-bg">
 	<div class="section-title">
-        <h4><?php _e( "Tweets", 'onesocial' ) ?></h4>
+        <a href="https://twitter.com/CitznsClimateUK">
+            <h4><?php _e( "Twitter", 'onesocial' ) ?></h4>
+        </a>
     </div>
-    <?php echo do_shortcode('[custom-twitter-feeds]') ?>
+    <div class="section-content">
+        <a class="twitter-timeline" data-tweet-limit="1" data-width="400" href="https://twitter.com/CitznsClimateUK?ref_src=twsrc%5Etfw">Tweets by CitznsClimateUK</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
 </section>
-<?php endif; ?>
