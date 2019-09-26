5453f24124c6dc92c9201df097dd6f9ca2d5fc63
Paul Jenkins <paul@turnpiece.com>
2019-09-26 19:09:56 +0100

    single posts

--- wp-content/themes/onesocial-ccluk/assets/js/menu.js
+++ wp-content/themes/onesocial-ccluk/assets/js/menu.js
@@ -1,11 +1,20 @@
 (function( $ ){
 
 	$( window ).load( function() {
-	    $( '#mobile-right-panel .menu-item-has-children' ).each( function () {
-	    	/* close submenu */
-	        $( this ).find( '.submenu-btn.fa-angle-down' ).trigger( 'click' );
-	    } );
+		var $panel = $( '#mobile-right-panel' );
 
+		if ($panel.length) {
+			$panel.find( '.menu-item-has-children' ).each( function () {
+				/* close submenu */
+				if ($( this ).hasClass( 'current-menu-item' ) ||
+					$( this ).hasClass( 'current-menu-parent') ||
+					$( this ).hasClass( 'current-menu-ancestor' )) {
+					
+					$( this ).find( '.submenu-btn.fa-angle-left' ).trigger( 'click' );
+				} else
+					$( this ).find( '.submenu-btn.fa-angle-down' ).trigger( 'click' );
+			} );
+		}
 	} );
 
 })( jQuery );
