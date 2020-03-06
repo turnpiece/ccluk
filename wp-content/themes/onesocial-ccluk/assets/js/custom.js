e9786e8f442ad72938b86d43abb169e6d867f9b2
Paul Jenkins <paul@turnpiece.com>
2020-03-06 23:01:46 +0000

    styling group settings page and refactoring styles

--- wp-content/themes/onesocial-ccluk/assets/js/custom.js
+++ wp-content/themes/onesocial-ccluk/assets/js/custom.js
@@ -13,6 +13,21 @@
     $(path+' p, '+path+' h2').each(function(){
         var string = $(this).html();
         $(this).html(string.replace(/\s(?=[^\s]*$)/g, "&nbsp;").replace(/\s(?=[^\s]*$)/g, "&nbsp;"));
-    });
+	});
+
+	function checkRadioLabels() {
+		$('div.radio').find('label').each( function() {
+			if ($(this).find('input').is(':checked'))
+				$(this).addClass('selected');
+			else
+				$(this).removeClass('selected');
+		})
+	}
+
+	$('div.radio').find('label').find('input').on('click', function() {
+		checkRadioLabels();
+	});
+
+	checkRadioLabels();
 
 })( jQuery );
