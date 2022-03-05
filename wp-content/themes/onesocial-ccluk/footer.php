c31476fb3f62bc376378d4efc78538e4cee42758
Paul Jenkins <paul@turnpiece.com>
2022-03-05 18:35:53 +0000

    remove options from footer.php

--- wp-content/themes/onesocial-ccluk/footer.php
+++ wp-content/themes/onesocial-ccluk/footer.php
@@ -23,89 +23,44 @@
 <?php
 global $bp, $post;
 
-$post_infinite			 = onesocial_get_option( 'post_infinite' );
-$boss_activity_infinite	 = onesocial_get_option( 'boss_activity_infinite' );
-$activity				 = isset( $bp ) ? $bp->current_component : false;
-
 // don't remove this filter, marketplace plugin uses it
-$show_footer = apply_filters( 'onesocial_show_footer', !( ( is_archive() || is_home() ) && $post_infinite && !( function_exists( 'is_shop' ) && is_shop() ) && !( function_exists( 'is_product_category' ) && is_product_category() ) && !( function_exists( 'is_product_tag' ) && is_product_tag() ) && !( function_exists( 'bbp_is_forum_archive' ) && bbp_is_forum_archive() ) ) || ( $activity == 'activity' && $boss_activity_infinite ) );
+$show_footer = apply_filters( 'onesocial_show_footer', true );
 
-if ( $show_footer ) {
-
-	$style = onesocial_get_option( 'onesocial_footer' );
-	?>
+if ( $show_footer ) : ?>
 
 	<footer id="colophon" class="<?php echo $style; ?>">
 
 		<?php get_template_part( 'template-parts/footer', 'widgets' ); ?>
 
-		<?php if ( 'footer-style-1' == $style ): ?>
+		<div class="footer-inner-bottom">
+			<div class="footer-inner">
 
-		    <div class="footer-inner-bottom">
-		        <div class="footer-inner">
+				<div id="footer-links">
 
-		            <div id="footer-links">
+					<div class="footer-credits <?php if ( !has_nav_menu( 'secondary-menu' ) ) : ?>footer-credits-single<?php endif; ?>">
+						&copy; 2022 - <?php echo get_bloginfo( 'name' ) ?>
+					</div>
 
-						<?php
-						$show_copyright	 = onesocial_get_option( 'footer_copyright_content' );
-						$copyright		 = onesocial_get_option( 'boss_copyright' );
+					<?php if ( has_nav_menu( 'secondary-menu' ) ) : ?>
+						<ul class="footer-menu">
+							<?php wp_nav_menu( array( 'container' => false, 'menu_id' => 'nav', 'theme_location' => 'secondary-menu', 'items_wrap' => '%3$s', 'depth' => 1, ) ); ?>
+						</ul>
+					<?php endif; ?>
 
-						if ( $show_copyright && $copyright ) {
-							?>
+				</div>
 
-							<div class="footer-credits <?php if ( !has_nav_menu( 'secondary-menu' ) ) : ?>footer-credits-single<?php endif; ?>">
-								<?php echo $copyright; ?>
-							</div>
+				<?php get_template_part( 'template-parts/footer-social-links' ); ?>
 
-						<?php } ?>
+			</div><!-- .footer-inner -->
 
-						<?php if ( has_nav_menu( 'secondary-menu' ) ) : ?>
-							<ul class="footer-menu">
-								<?php wp_nav_menu( array( 'container' => false, 'menu_id' => 'nav', 'theme_location' => 'secondary-menu', 'items_wrap' => '%3$s', 'depth' => 1, ) ); ?>
-							</ul>
-						<?php endif; ?>
-
-		            </div>
-
-					<?php if ( onesocial_get_option( 'boss_layout_switcher' ) ) { ?>
-						<form id="switch-mode" name="switch-mode" method="post" action="">
-							<input type="submit" value="View as Desktop" tabindex="1" id="switch_submit" name="submit" />
-							<input type="hidden" id="switch_mode" name="switch_mode" value="desktop" />
-							<?php wp_nonce_field( 'switcher_action', 'switcher_nonce_field' ); ?>
-						</form>
-					<?php } ?>
-
-					<?php get_template_part( 'template-parts/footer-social-links' ); ?>
-
-				</div><!-- .footer-inner -->
-
-		    </div><!-- .footer-inner-bottom -->
-
-		<?php endif; ?>
+		</div><!-- .footer-inner-bottom -->
 
 		<!-- Don't delete this -->
 		<div class="bb-overlay"></div>
 
 	</footer>
 
-<?php } ?>
-
-<?php
-if ( !is_user_logged_in() ) {
-
-	if ( onesocial_get_option( 'user_login_option' ) ) {
-
-		if ( buddyboss_is_bp_active() && bp_get_signup_allowed() ) {
-			get_template_part( 'template-parts/site-register' );
-		}
-
-		get_template_part( 'template-parts/site-login' );
-	}
-}
-
-// Lost Password
-get_template_part( 'template-parts/site-lost-password' );
-?>
+<?php endif; ?>
 
 <?php do_action( 'bp_footer' ) ?>
 
