ebc0cbcbf11fed4d8d8f9bfe99a28b533a12a362
Paul Jenkins <paul@turnpiece.com>
2025-05-13 11:04:01 +0100

    mobile web app upgrade

--- wp-content/themes/ccluk/header.php
+++ wp-content/themes/ccluk/header.php
@@ -16,7 +16,7 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="mobile-web-app-capable" content="yes" />
 	<meta name="msapplication-tap-highlight" content="no" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
