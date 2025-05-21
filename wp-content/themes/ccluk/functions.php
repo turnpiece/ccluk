0c65239f475a0d499418a22ac26df805332eb003
Paul Jenkins <paul@turnpiece.com>
2025-05-21 18:54:58 +0100

    remove commenting

--- wp-content/themes/ccluk/functions.php
+++ wp-content/themes/ccluk/functions.php
@@ -286,3 +286,25 @@ function ccluk_vardump($array)
 {
     ccluk_debug(print_r($array, true));
 }
+
+/**
+ * Disable comments globally
+ */
+// Close comments on the front-end
+add_filter('comments_open', '__return_false', 20, 2);
+add_filter('pings_open', '__return_false', 20, 2);
+
+// Hide existing comments
+add_filter('comments_array', '__return_empty_array', 10, 2);
+
+// Remove comments page from admin menu
+add_action('admin_menu', function() {
+    remove_menu_page('edit-comments.php');
+    remove_submenu_page('options-general.php', 'options-discussion.php');
+});
+
+// Remove comments links from admin bar
+add_action('wp_before_admin_bar_render', function() {
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu('comments');
+});
